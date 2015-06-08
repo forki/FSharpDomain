a06ff2f8d3cbddf5af988115617eab56042379f9
info@millbank <in@magsolutionslimited.co.uk>
2015-06-08 18:20:31 +0100

    Initial Commit

---
+++ src/FSharpDomain/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FSharpDomain")>]
+[<assembly: AssemblyProductAttribute("FSharpDomain")>]
+[<assembly: AssemblyDescriptionAttribute("Demonstration of F# being used in a C# project")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
